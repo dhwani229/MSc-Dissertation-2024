8b097a33ec99667d9b7f9fc8a112513a205c75b0
dhwani229 <157715542+dhwani229@users.noreply.github.com>
2024-04-26 21:48:04 +0100

    Create fq.sh
    conversion of .sra to .fastq. this e.g. involves dealing with RRBS data but will be similar for WGBS data too

---
+++ fq.sh
@@ -0,0 +1,24 @@
+# jobscript for converting the .sra files into .fastq
+
+#!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -pe smp 1
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=4G
+
+module load sratools/2.10.8
+
+#for SRA_DIR in SRR*/
+path=/data/scratch/bt22912/prac
+
+for SRA_DIR in $(ls ${path} | grep SRR)
+do
+    cd ${path}/${SRA_DIR}/   
+    
+    fasterq-dump "${SRA_DIR}" --split-files --threads 1
+    
+
+    
+    cd ..
+done
