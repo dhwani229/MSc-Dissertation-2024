5c03c5f857d3fed05923835babdcd19a4dee9545
dhwani229 <157715542+dhwani229@users.noreply.github.com>
2024-05-07 11:51:34 +0100

    Update fq.sh

--- RRBS_jobscripts/fq.sh
+++ RRBS_jobscripts/fq.sh
@@ -3,9 +3,10 @@
 #!/bin/bash
 #$ -cwd
 #$ -j y
-#$ -pe smp 1
-#$ -l h_rt=48:00:00
-#$ -l h_vmem=4G
+#$ -pe smp 4
+#$ -l h_rt=240:00:00
+#$ -l h_vmem=15G
+#$ -m bea
 
 module load sratools/2.10.8
 
@@ -16,9 +17,8 @@ for SRA_DIR in $(ls ${path} | grep SRR)
 do
     cd ${path}/${SRA_DIR}/   
     
-    fasterq-dump "${SRA_DIR}" --split-files --threads 1
-    
+    fasterq-dump "${SRA_DIR}" --split-files --threads ${NSLOTS}    
 
     
     cd ..
-done 
+done
