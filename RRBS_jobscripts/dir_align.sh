ce96f2ae62b17abaa380388aef2d68a79a4315b7
dhwani229 <157715542+dhwani229@users.noreply.github.com>
2024-05-07 16:02:15 +0100

    Create dir_align.sh

---
+++ RRBS_jobscripts/dir_align.sh
@@ -0,0 +1,22 @@
+# make alignment directory inside sample directory 
+
+#!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -pe smp 1
+#$ -l h_rt=1:00:00
+#$ -l h_vmem=1G
+
+path=/data/scratch/bt22912/prac
+
+
+for SRA_DIR in $(ls ${path} | grep SRR)
+do
+    
+    cd ${path}/${SRA_DIR}/
+    
+    mkdir alignment
+    
+    
+    cd ..
+done
